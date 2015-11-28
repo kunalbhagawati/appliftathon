67c49df7a91bcdc321c2ed20e6c7cce64f90840b
kunalb <kunal.bhagawati@yahoo.com>
2015-11-28 18:41:17 +0530

    Add redux

--- dojohackers/assets/js/index.js
+++ dojohackers/assets/js/index.js
@@ -1,5 +1,6 @@
-var ReactDOM = require('react-dom');
-var App = require('./app');
+var React = require('react');
+var App = require('./app/app.js');
+require('../css/main.css');
 
 
-ReactDOM.render(<App/>, document.getElementById('react-app'));
+React.render(<App/>, document.getElementById('app-container'));
--- dojohackers/webpack.config.js
+++ dojohackers/webpack.config.js
@@ -17,9 +17,30 @@ module.exports = {
     ],
 
     module: {
+        //postLoaders: [
+        //    {
+        //        test: /\.js$/, // include .js files
+        //        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        //        loader: "jshint-loader"
+        //    }
+        //],
+
         loaders: [
-            {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'} // to transform JSX into JS
+            {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},  // to transform JSX into JS
+            {test: /\.css$/, loader: "style!css"}
         ]
+    },
+
+    // more options in the optional jshint object
+    jshint: {
+        // jshint errors are displayed by default as warnings
+        // set emitErrors to true to display them as errors
+        emitErrors: false,
+
+        // jshint to not interrupt the compilation
+        // if you want any file with jshint errors to fail
+        // set failOnHint to true
+        failOnHint: true
     },
 
     resolve: {
