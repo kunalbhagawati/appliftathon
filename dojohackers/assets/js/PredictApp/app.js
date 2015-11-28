bd58a1a123af1d14da9f55c52d100a292137ee43
kunalb <kunal.bhagawati@yahoo.com>
2015-11-29 00:14:28 +0530

    Add jquery and add file upload

--- dojohackers/assets/js/PredictApp/app.js
+++ dojohackers/assets/js/PredictApp/app.js
@@ -1,7 +1,30 @@
 var React = require('react');
+var $ = require('jQuery');
+
 
 module.exports = React.createClass({
+    postFile: function() {
+        $.ajax()
+    },
+
+    renderValuesForm: function() {
+        return null;
+    },
+
+    renderFileUploadField: function() {
+        return <input type="file" onchange={this.postFile} />;
+    },
+
+    renderInputTypes: function() {
+        return (
+            <div>
+                {this.renderValuesForm()}
+                {this.renderFileUploadField()}
+            </div>
+        )
+    },
+
     render: function() {
-        return <h1>Predict</h1>
+        return this.renderInputTypes();
     }
 });
